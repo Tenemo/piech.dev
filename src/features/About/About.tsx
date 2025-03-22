f503014ecded16502719d78c8937927c1fb11493
Tenemo <tenemo@gmail.com>
2025-03-23 00:28:57 +0100

    about page first draft

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router';
+
+import styles from './about.module.scss';
 
 const About = (): React.JSX.Element => {
     return (
-        <main>
-            <h2>About Me</h2>
-            <p>short paragraph about me</p>
+        <main className={styles.about}>
+            <h2>About me</h2>
+            <div className={styles.divider} />
+            <p>[TODO: short paragraph about me]</p>
+
+            <div className={styles.buttonsContainer}>
+                <Link className={styles.mainButton} to="/portfolio">
+                    Check out my portfolio
+                </Link>
+
+                <div className={styles.dividerText}>OR</div>
+
+                <Link className={styles.contactButton} to="/contact">
+                    Contact me
+                </Link>
+            </div>
         </main>
     );
 };

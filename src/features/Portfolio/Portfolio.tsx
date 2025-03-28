a9504a38355380d708606317c0ccf2f76bfe9915
Tenemo <tenemo@gmail.com>
2025-03-28 18:20:32 +0100

    no more <a> nesting

--- src/features/Portfolio/Portfolio.tsx
+++ src/features/Portfolio/Portfolio.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import styles from './portfolio.module.scss';
 import PortfolioCard from './PortfolioCard/PortfolioCard';
@@ -11,43 +10,41 @@ const Portfolio = (): React.JSX.Element => {
             <div className={'divider'} />
 
             <div className={styles.portfolioItemsContainer}>
-                <Link to="/portfolio/reactplate">
-                    <PortfolioCard
-                        imageAlt="Reactplate project and its lighthouse scores"
-                        imageUrl="src/images/projects/reactplate_lighthouse.png"
-                        technologies={[
-                            'typescript',
-                            'react',
-                            'redux',
-                            'sass',
-                            'vitejs',
-                            'eslint',
-                            'prettier',
-                        ]}
-                    >
-                        <h3>Reactplate</h3>
-                        <p>A React template. TODO: longer description</p>
-                    </PortfolioCard>
-                </Link>
+                <PortfolioCard
+                    imageAlt="Reactplate project and its lighthouse scores"
+                    imageUrl="src/images/projects/reactplate_lighthouse.png"
+                    project="reactplate"
+                    technologies={[
+                        'typescript',
+                        'react',
+                        'redux',
+                        'sass',
+                        'vitejs',
+                        'eslint',
+                        'prettier',
+                    ]}
+                >
+                    <h3>Reactplate</h3>
+                    <p>A React template. TODO: longer description</p>
+                </PortfolioCard>
 
-                <Link to="/portfolio/expressplate">
-                    <PortfolioCard
-                        imageAlt="Expressplate project"
-                        imageOnRight={true}
-                        imageUrl="/path/to/image2.jpg"
-                        technologies={[
-                            'nodejs',
-                            'typescript',
-                            'express',
-                            'vitejs',
-                            'eslint',
-                            'prettier',
-                        ]}
-                    >
-                        <h3>Expressplate</h3>
-                        <p>An Express.js template. TODO: longer description</p>
-                    </PortfolioCard>
-                </Link>
+                <PortfolioCard
+                    imageAlt="Expressplate project"
+                    imageOnRight={true}
+                    imageUrl="/path/to/image2.jpg"
+                    project="expressplate"
+                    technologies={[
+                        'nodejs',
+                        'typescript',
+                        'express',
+                        'vitejs',
+                        'eslint',
+                        'prettier',
+                    ]}
+                >
+                    <h3>Expressplate</h3>
+                    <p>An Express.js template. TODO: longer description</p>
+                </PortfolioCard>
             </div>
         </main>
     );
--- src/features/Portfolio/PortfolioCard/PortfolioCard.tsx
+++ src/features/Portfolio/PortfolioCard/PortfolioCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import { TECHNOLOGIES } from '../technologies';
 
@@ -10,6 +11,7 @@ type PortfolioCardProps = {
     imageAlt: string;
     imageOnRight?: boolean;
     technologies: string[];
+    project: string;
 };
 
 const PortfolioCard = ({
@@ -18,46 +20,53 @@ const PortfolioCard = ({
     imageAlt,
     imageOnRight = false,
     technologies,
+    project,
 }: PortfolioCardProps): React.JSX.Element => {
     return (
         <div
             className={`${styles.card} ${imageOnRight ? styles.imageRight : styles.imageLeft}`}
         >
-            <div className={styles.imageContainer}>
-                <img alt={imageAlt} className={styles.image} src={imageUrl} />
-            </div>
-            <div className={styles.content}>
-                {children}
+            <>
+                <div className={styles.imageContainer}>
+                    <img
+                        alt={imageAlt}
+                        className={styles.image}
+                        src={imageUrl}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <Link to={`/portfolio/${project}`}>{children}</Link>
 
-                {technologies.length > 0 && (
-                    <div className={styles.technologiesContainer}>
-                        {technologies.map((technologyName) => {
-                            const technologyInfo =
-                                TECHNOLOGIES[
-                                    technologyName as keyof typeof TECHNOLOGIES
-                                ];
-                            return (
-                                <a
-                                    href={technologyInfo.url}
-                                    key={technologyName}
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                    }}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    title={technologyInfo.fullName}
-                                >
-                                    <img
-                                        alt={`${technologyName} logo`}
-                                        className={styles.techLogo}
-                                        src={`src/images/logos/${technologyName}_logo.png`}
-                                    />
-                                </a>
-                            );
-                        })}
-                    </div>
-                )}
-            </div>
+                    {technologies.length > 0 && (
+                        <div className={styles.technologiesContainer}>
+                            {technologies.map((technologyName) => {
+                                const technologyInfo =
+                                    TECHNOLOGIES[
+                                        technologyName as keyof typeof TECHNOLOGIES
+                                    ];
+                                return (
+                                    <a
+                                        href={technologyInfo.url}
+                                        key={technologyName}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                        }}
+                                        rel="noopener noreferrer"
+                                        target="_blank"
+                                        title={technologyInfo.fullName}
+                                    >
+                                        <img
+                                            alt={`${technologyName} logo`}
+                                            className={styles.techLogo}
+                                            src={`src/images/logos/${technologyName}_logo.png`}
+                                        />
+                                    </a>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
+            </>
         </div>
     );
 };

9d74969dd40f8fd105c4f2aab8477e0a74b300fa
Tenemo <piotr@piech.dev>
2025-10-15 05:40:39 +0200

    rename leftovers cleaned up

--- src/features/Projects/ProjectCard/ProjectCard.tsx
+++ src/features/Projects/ProjectCard/ProjectCard.tsx
@@ -7,7 +7,7 @@ import { TECHNOLOGIES } from '../technologies';
 import styles from './projectCard.module.scss';
 import ProjectTechnologies from './ProjectTechnologies/ProjectTechnologies';
 
-import { REPOSITORY_INFO } from 'utils/githubDatazzz';
+import { REPOSITORY_INFO } from 'utils/githubData';
 
 type ProjectCardProps = {
     projectPreview: string;

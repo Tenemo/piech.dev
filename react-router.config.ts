93487e3c25f13dcfe96e3a44399a188a985653c2
Tenemo <piotr@piech.dev>
2025-10-15 05:49:55 +0200

    gitHub -> github, last remaining pieces

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,14 +3,14 @@ import 'dotenv/config';
 import type { Config } from '@react-router/dev/config';
 
 import { PROJECTS } from './src/features/Projects/projectsList';
-import { fetchGitHubData } from './src/utils/fetchGithubData';
+import { fetchGithubData } from './src/utils/fetchGithubData';
 
 export default {
     appDirectory: 'src',
     buildDirectory: 'dist',
     ssr: true,
     async prerender() {
-        await fetchGitHubData({ refetch: false });
+        await fetchGithubData({ refetch: false });
         const staticPaths = ['/', '/projects', '/contact'];
         const itemPaths = PROJECTS.map(
             (p) => `/projects/${p.repoName ?? p.project}`,

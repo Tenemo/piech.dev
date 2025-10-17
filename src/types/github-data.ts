a245d5b5a9646ab0d303cad39fa5d4fa91dcd232
Tenemo <piotr@piech.dev>
2025-10-17 18:32:56 +0200

    createdDatetime for repos

--- src/types/github-data.ts
+++ src/types/github-data.ts
@@ -2,10 +2,11 @@ export type RepositoryInfo = {
     name: string;
     description: string;
     topics?: string[];
+    createdDatetime: string;
 };
 
 export type GithubData = {
-    METADATA?: { datetimeFetched: string };
+    METADATA?: { fetchedDatetime: string };
     REPOSITORY_INFO: Partial<Record<string, RepositoryInfo>>;
     README_CONTENT: Partial<Record<string, string>>;
 };
